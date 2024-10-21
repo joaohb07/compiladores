42453e3bf111f456edebe7d409b3fcfc66f20b3c
joaohb07 <joaoh072002@gmail.com>
2024-10-20 23:09:14 -0300

    isnum update

--- mybc_1/lexer.c
+++ mybc_1/lexer.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <lexer.h>
 
-char lexeme[MAXIDLEN + 1];
+char lexeme[MAXIDLEN + 1];  // Armazena o valor numérico lido como string
 
 // ID = [A-Za-z][A-Za-z0-9]*
 int isID(FILE *tape)
@@ -49,7 +49,6 @@ int isASGN(FILE *tape)
         lexeme[i] = getc(tape);
         if (lexeme[i] == '=')
         {
-            // ungetc(lexeme[i], tape);
             lexeme[i] = 0;
             return ASGN;
         }
@@ -62,159 +61,55 @@ int isASGN(FILE *tape)
     return 0;
 }
 
-// // DEC = [1-9][0-9]* | 0
-int isDEC(FILE *tape)
-{
-    int i = 0;
-
-    if (isdigit(lexeme[i] = getc(tape)))
-    {
-        ++i;
-        if (lexeme[i] == '0')
-        {
-            return DEC;
-        }
-        while (isdigit(lexeme[i] = getc(tape)))
-            ++i;
-
-        ungetc(lexeme[i], tape);
-        lexeme[i] = 0;
-        return NUM;
-    }
-    ungetc(lexeme[i], tape);
-    lexeme[i] = 0;
-    return 0;
-}
-
 int isNUM(FILE *tape)
 {
     int i = 0;
+    char c;
 
-    if (isdigit(lexeme[i] = getc(tape)))
+    // Primeira leitura de dígitos ou ponto
+    c = getc(tape);
+    if (isdigit(c) || c == '.')
     {
-        ++i;
-        if (lexeme[i] == '0')
-        {
+        lexeme[i++] = c;
 
-            return DEC;
+        // Continua lendo dígitos
+        while ((c = getc(tape)) && (isdigit(c) || c == '.'))
+        {
+            lexeme[i++] = c;
         }
-        while (isdigit(lexeme[i] = getc(tape)))
-            ++i;
 
-        // Verifica se o numero é um ponto flutuante
-        if (lexeme[i] == '.')
+        // Verifica se encontrou notação científica
+        if (c == 'e' || c == 'E')
         {
-            ++i;
-            while (isdigit(lexeme[i] = getc(tape)))
-                ++i;
+            lexeme[i++] = c;
+            c = getc(tape);
 
-            // |"e"("+"|"-")[0-9]*)
-            if (lexeme[i] == 'e')
+            // Pode ser seguido de '+' ou '-' no expoente
+            if (c == '+' || c == '-')
             {
-                ++i;
-                lexeme[i] = getc(tape);
-                if (lexeme[i] == '+')
-                {
-                    ++i;
-                    while (isdigit(lexeme[i] = getc(tape)))
-                        ++i;
-
-                    ungetc(lexeme[i], tape);
-                    lexeme[i] = 0;
-                    return NUM;
-                }
-                if (lexeme[i] == '-')
-                {
-                    ++i;
-                    while (isdigit(lexeme[i] = getc(tape)))
-                        ++i;
-
-                    ungetc(lexeme[i], tape);
-                    lexeme[i] = 0;
-                    return NUM;
-                }
+                lexeme[i++] = c;
+                c = getc(tape);
             }
 
-            ungetc(lexeme[i], tape);
-            lexeme[i] = 0;
-            return NUM;
+            // Continua lendo os dígitos do expoente
+            while (isdigit(c))
+            {
+                lexeme[i++] = c;
+                c = getc(tape);
+            }
         }
 
-        ungetc(lexeme[i], tape);
-        lexeme[i] = 0;
+        ungetc(c, tape); // Devolve o último caractere não numérico
+        lexeme[i] = '\0'; // Finaliza a string em lexeme
         return NUM;
     }
 
-    if (lexeme[i] == '.')
-    {
-        lexeme[i] = '0';
-        ++i;
-        lexeme[i] = '.';
-        ++i;
-        while (isdigit(lexeme[i] = getc(tape)))
-            ++i;
-
-        ungetc(lexeme[i], tape);
-        lexeme[i] = 0;
-        return NUM;
-    }
-
-    ungetc(lexeme[i], tape);
-    lexeme[i] = 0;
+    // Se não for um número válido, devolve o caractere lido e retorna 0
+    ungetc(c, tape);
+    lexeme[0] = '\0';
     return 0;
 }
 
-/*
-
-// OCT = 0[0-7]+
-int isOCT(FILE *tape)
-{
-    int i = 0;
-    lexeme[i] = getc(tape);
-    if (lexeme[i] == '0')
-    {
-        ++i;
-        lexeme[i] = getc(tape);
-        while (lexeme[i] >= '0' && lexeme[i] <= '7')
-        {
-            ++i;
-            lexeme[i] = getc(tape);
-        }
-        ungetc(lexeme[i], tape);
-        lexeme[i] = 0;
-        return OCT;
-    }
-    ungetc(lexeme[i], tape);
-    lexeme[i] = 0;
-    return 0;
-}
-
-// HEX = 0(x|X)[0-9a-fA-F]+
-int isHEX(FILE *tape)
-{
-    int i = 0;
-    lexeme[i] = getc(tape);
-    if (lexeme[i] == '0')
-    {
-        ++i;
-        lexeme[i] = getc(tape);
-        if (lexeme[i] == 'x' || lexeme[i] == 'X')
-        {
-            ++i;
-            while (isxdigit(lexeme[i] = getc(tape)))
-                ++i;
-            ungetc(lexeme[i], tape);
-            lexeme[i] = 0;
-            return HEX;
-        }
-        ungetc(lexeme[i], tape);
-        --i;
-    }
-    ungetc(lexeme[i], tape);
-    lexeme[i] = 0;
-    return 0;
-}
-*/
 void skipspaces(FILE *tape)
 {
     int head;
@@ -239,12 +134,10 @@ int gettoken(FILE *source)
     }
     if ((token = isNUM(source)))
     {
-
         return token;
     }
     if ((token = isASGN(source)))
     {
-
         return token;
     }
     token = getc(source);
